35b58b84df9c5107315130d9a4e2ac34fc5a6c21
changdechuang <15264086654@163.com>
2019-05-24 12:39:37 +0800

    c

--- todolist/app.js
+++ todolist/app.js
@@ -1,20 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+
 var fs = require('fs')
 var app = express();
 var todoController = require('./controllers/todoController');
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
 
 app.set('view engine', 'ejs');
 
-app.use('/assets',express.static('./assets'));
+app.use(express.static('./'));
 
 todoController(app);
 
-app.get('/', function(req, res) {
-  res.render('todo', { title: req.params.title, message: req.params.message});
-});
+
 app.listen(3000);
 
 console.log('You are listening to port 3000');
--- todolist/controllers/todoController.js
+++ todolist/controllers/todoController.js
@@ -1,13 +1,31 @@
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+var data = [{item:'get milk'},{item:'walk dog '},{item:'coding'}]
+
+
 module.exports = function(app) {
   app.get('/todo', function(req, res) {
-
+    res.render('todo', {todos:data});
   });
 
-  app.post('/todo', function(req, res) {
-
+  app.post('/todo',urlencodedParser, function(req, res) {
+    
+    data.push(req.body)
+    res.render('todo', {todos:data});
   });
 
-  app.delete('/todo', function(req, res) {
-
+  app.delete('/todo/:item', function(req, res) {
+    
+    data.forEach((arr,index)=>{
+      if(arr.item.trim().replace(/ /g, "-")==req.params.item){
+        
+        data.splice(index, 1); 
+        res.render('todo', {todos:data});
+      }
+    })
+    
   });
 }
