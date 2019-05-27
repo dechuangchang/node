b23a5e37d5ac5a724409a987b3fb94ee58f05551
changdechuang <15264086654@163.com>
2019-05-27 16:40:08 +0800

    sql

--- todolist/controllers/todoController.js
+++ todolist/controllers/todoController.js
@@ -1,7 +1,19 @@
 var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+var mongoose = require('mongoose');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+mongoose.connect('mongodb+srv://admin:<admin>@chang-adt1z.mongodb.net/test?retryWrites=true',{useNewUrlParser: true});
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+var myModel = mongoose.model('chang');
+console.log(myModel)
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err) {
+//   if (err) throw err;
+//   console.log('item saved');
+// });
+// console.log(itemOne)
 
 var data = [{item:'get milk'},{item:'walk dog '},{item:'coding'}]
 
@@ -14,7 +26,7 @@ module.exports = function(app) {
   app.post('/todo',urlencodedParser, function(req, res) {
     
     data.push(req.body)
-    res.render('todo', {todos:data});
+    res.json(data);
   });
 
   app.delete('/todo/:item', function(req, res) {
@@ -23,9 +35,10 @@ module.exports = function(app) {
       if(arr.item.trim().replace(/ /g, "-")==req.params.item){
         
         data.splice(index, 1); 
-        res.render('todo', {todos:data});
+        res.json( data);
       }
     })
     
   });
 }
+// 
